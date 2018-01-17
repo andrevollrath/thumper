9c23e910c7f279b4c4bdf3164350277849e302a9
drew <andre@vollrath.ca>
2018-01-16 23:28:36 -0800

    Slowed down thumping

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,7 +21,7 @@ $(document).ready(function() {
   $('#thump_box').on('click', function (e) {
 
     if ($(this).hasClass('active')){
-      var num = Math.floor(Math.random() * (100 - 33 + 1)) + 33;;
+      var num = Math.floor(Math.random() * (200 - 75 + 1)) + 75;;
 
       $(this).removeClass('active');
       $(this).html('Thumping it <span id="count">0</span> / ' + num);  
--- app/controllers/thumps_controller.rb
+++ app/controllers/thumps_controller.rb
@@ -3,7 +3,7 @@ class ThumpsController < ApplicationController
   def index; end
 
   def it
-    sleep(rand(1..3))
+    sleep(rand(10..30))
 
     mech = Mechanize.new {|agent| agent.user_agent_alias = 'Mac Safari'}
     page = mech.get('https://classifieds.castanet.net/details/local_experienced_bookkeeping_team/3341903/')
